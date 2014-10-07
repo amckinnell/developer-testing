2920b9856a85b9a4a304c0b54ac78b6feedb953d
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-10-07 00:41:01 -0400

    Simplifies Backstage Pass leg.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -20,13 +20,11 @@ class GildedRose
       increase_quality(item)
     elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
       increase_quality(item)
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.sell_in < 10
-          increase_quality(item)
-        end
-        if item.sell_in < 5
-          increase_quality(item)
-        end
+      if item.sell_in < 10
+        increase_quality(item)
+      end
+      if item.sell_in < 5
+        increase_quality(item)
       end
     else
       decrease_quality(item)
