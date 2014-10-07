9f74923ab62eb73f5392a59e59f0444a86388043
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-10-07 00:40:37 -0400

    Simplifies Aged Brie leg.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,14 +18,6 @@ class GildedRose
 
     if item.name == 'Aged Brie'
       increase_quality(item)
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.sell_in < 10
-          increase_quality(item)
-        end
-        if item.sell_in < 5
-          increase_quality(item)
-        end
-      end
     elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
       increase_quality(item)
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
