92b68c20fa0da1282f62ed167f692bcf2a31bed8
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-10-07 00:36:17 -0400

    Extracts the #decrease_quality method.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,9 +17,7 @@ class GildedRose
     item.sell_in = item.sell_in - 1
 
     if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        item.quality = item.quality - 1
-      end
+      decrease_quality(item)
     else
       increase_quality(item)
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
@@ -35,15 +33,19 @@ class GildedRose
     if item.sell_in < 0
       if item.name != 'Aged Brie'
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
+          decrease_quality(item)
         else
           item.quality = item.quality - item.quality
         end
       else
         increase_quality(item)
       end
+    end
+  end
+
+  def decrease_quality(item)
+    if item.quality > 0
+      item.quality = item.quality - 1
     end
   end
 
