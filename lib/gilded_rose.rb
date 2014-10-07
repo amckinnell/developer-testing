7cd0d783aff1cd4a181eb95282385d3ff827889e
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-10-07 00:36:59 -0400

    Uses the -= and += operators to simplify.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -14,7 +14,7 @@ class GildedRose
   def update_item_quality(item)
     return if item.name == 'Sulfuras, Hand of Ragnaros'
 
-    item.sell_in = item.sell_in - 1
+    item.sell_in -= 1
 
     if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
       decrease_quality(item)
@@ -45,13 +45,13 @@ class GildedRose
 
   def decrease_quality(item)
     if item.quality > 0
-      item.quality = item.quality - 1
+      item.quality -= 1
     end
   end
 
   def increase_quality(item)
     if item.quality < 50
-      item.quality = item.quality + 1
+      item.quality += 1
     end
   end
 end
