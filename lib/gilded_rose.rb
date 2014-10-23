29646a479a8b6f8eccca6fd9433023ce31081728
Michael Atlin <mike.atlin@gmail.com>
2014-10-23 16:26:15 -0400

    Bring back to unrefactored code, plus charactarization file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,71 +1,57 @@
 class GildedRose
   attr_reader :items
-
-  def initialize(items)
-    @items = items
+  
+  def initialize item_attributes
+    @items = item_attributes.map { |args| Item.new(*args) }
   end
 
   def update_quality
-    @items.each { |item| update_item_quality(item) }
-  end
 
-  private
-
-  def update_item_quality(item)
-    return if item.name == 'Sulfuras, Hand of Ragnaros'
-
-    perform_inventory_rollover(item)
-    perform_inventory_expiration(item)
-  end
-
-  def perform_inventory_rollover(item)
-    item.sell_in -= 1
-
-    case item.name
-    when 'Aged Brie'
-      increase_quality(item)
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      increase_quality(item)
-      increase_quality(item) if item.sell_in < 10
-      increase_quality(item) if item.sell_in < 5
-    when 'Conjured Mana'
-      decrease_quality(item)
-      decrease_quality(item)
-    else
-      decrease_quality(item)
+    for i in 0..(@items.size-1)
+      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (@items[i].quality > 0)
+          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+            @items[i].quality = @items[i].quality - 1
+          end
+        end
+      else
+        if (@items[i].quality < 50)
+          @items[i].quality = @items[i].quality + 1
+          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+            if (@items[i].sell_in < 11)
+              if (@items[i].quality < 50)
+                @items[i].quality = @items[i].quality + 1
+              end
+            end
+            if (@items[i].sell_in < 6)
+              if (@items[i].quality < 50)
+                @items[i].quality = @items[i].quality + 1
+              end
+            end
+          end
+        end
+      end
+      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+        @items[i].sell_in = @items[i].sell_in - 1;
+      end
+      if (@items[i].sell_in < 0)
+        if (@items[i].name != "Aged Brie")
+          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+            if (@items[i].quality > 0)
+              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+                @items[i].quality = @items[i].quality - 1
+              end
+            end
+          else
+            @items[i].quality = @items[i].quality - @items[i].quality
+          end
+        else
+          if (@items[i].quality < 50)
+            @items[i].quality = @items[i].quality + 1
+          end
+        end
+      end
     end
-  end
-
-  def perform_inventory_expiration(item)
-    return unless expired?(item)
-
-    case item.name
-    when 'Aged Brie'
-      increase_quality(item)
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      writeoff(item)
-    when 'Conjured Mana'
-      decrease_quality(item)
-      decrease_quality(item)
-    else
-      decrease_quality(item)
-    end
-  end
-
-  def expired?(item)
-    item.sell_in < 0
-  end
-
-  def decrease_quality(item)
-    item.quality -= 1 if 0 < item.quality
-  end
-
-  def increase_quality(item)
-    item.quality += 1 if item.quality < 50
-  end
-
-  def writeoff(item)
-    item.quality = 0
   end
 
 end
--- spec/gilded_rose_characterization_spec.rb
+++ spec/gilded_rose_characterization_spec.rb
@@ -7,7 +7,7 @@ require_relative '../lib/item'
 describe GildedRose do
 
   it 'knows how to update quality for items' do
-    subject = GildedRose.new(items)
+    subject = GildedRose.new(item_attributes)
 
     verify { characterize(subject, 20) }
   end
@@ -16,19 +16,18 @@ end
 
 private
 
-def items
+def item_attributes
   item_attributes = [
     ['Mail Armour', 10, 20],
     ['Mail Armour', 10, 1],
+    ["+5 Dexterity Vest", 10, 20],
+    ["Elixir of the Mongoose", 5, 7],
     ['Aged Brie', 4, 9],
     ['Aged Brie', 1, 49],
     ['Backstage passes to a TAFKAL80ETC concert', 15, 17],
     ['Backstage passes to a TAFKAL80ETC concert', 5, 49],
-    ['Sulfuras, Hand of Ragnaros', -1, 80],
-    ['Conjured Mana', 13, 50]
+    ['Sulfuras, Hand of Ragnaros', -1, 80]
   ]
-
-  item_attributes.map { |args| Item.new(*args) }
 end
 
 def characterize(subject, days)
