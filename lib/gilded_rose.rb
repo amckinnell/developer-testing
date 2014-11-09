f0c8f0cb70b3033d7ecd42757dfa62958b5a0773
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-11-09 14:37:08 -0500

    Untangles the reading and writing to the item.sell_in attribute.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,6 +12,8 @@ class GildedRose
   def increase_item_quality(item)
     return if item.name == 'Sulfuras, Hand of Ragnaros'
 
+    item.sell_in = item.sell_in - 1
+
     if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         item.quality = item.quality - 1
@@ -20,12 +22,12 @@ class GildedRose
       if item.quality < 50
         item.quality = item.quality + 1
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
+          if item.sell_in < 10
             if item.quality < 50
               item.quality = item.quality + 1
             end
           end
-          if item.sell_in < 6
+          if item.sell_in < 5
             if item.quality < 50
               item.quality = item.quality + 1
             end
@@ -33,8 +35,6 @@ class GildedRose
         end
       end
     end
-
-    item.sell_in = item.sell_in - 1
 
     if item.sell_in < 0
       if item.name != 'Aged Brie'
