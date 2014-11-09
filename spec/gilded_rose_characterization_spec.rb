3b23caa8e36807fee4b2dde9d6da29f8baee996f
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-11-09 12:13:30 -0500

    Adds another item.

--- spec/gilded_rose_characterization_spec.rb
+++ spec/gilded_rose_characterization_spec.rb
@@ -6,11 +6,15 @@ require_relative '../lib/item'
 describe GildedRose do
 
   it 'knows how to update quality for items' do
-    items = [Item.new('Mail Armour', 10, 20)]
+    items = [
+      Item.new('Mail Armour', 10, 20),
+      Item.new('Aged Brie', 4, 9)
+    ]
     subject = GildedRose.new(items)
 
     subject.update_quality
     expect(subject.items[0].to_s).to eq('Mail Armour, 9, 19')
+
 
     subject.update_quality
     expect(subject.items[0].to_s).to eq('Mail Armour, 8, 18')
