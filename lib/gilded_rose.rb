1bd5af6110434bd831ebbd078f260d0a953309d4
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-11-09 14:42:34 -0500

    Duplicates a leg of an if to prepare for simplification.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,6 +26,16 @@ class GildedRose
           increase_quality(item)
         end
       end
+    elsif item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      increase_quality(item)
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 10
+          increase_quality(item)
+        end
+        if item.sell_in < 5
+          increase_quality(item)
+        end
+      end
     else
       decrease_quality(item)
     end
