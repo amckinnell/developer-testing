9006410389ac7f3378f7964eca662aab21f1b226
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-11-12 05:35:24 -0500

    Turns the GildedRose methods and the InventoryItem inside out.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,47 +12,8 @@ class GildedRose
   private
 
   def update_item_quality(item)
-    perform_inventory_rollover(item)
-    perform_inventory_expiration(item)
-  end
-
-  def perform_inventory_rollover(item)
-    case item.name
-    when 'Aged Brie'
-      item.rollover
-      item.increase_quality
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      item.rollover
-      amount = case item.sell_in
-      when (0..4) then 3
-      when (5..9) then 2
-      else 1
-      end
-      item.increase_quality amount
-    when 'Conjured Mana'
-      item.rollover
-      item.decrease_quality 2
-    when 'Sulfuras, Hand of Ragnaros'
-      # Do nothing
-    else
-      item.rollover
-      item.decrease_quality
-    end
-  end
-
-  def perform_inventory_expiration(item)
-    case item.name
-    when 'Aged Brie'
-      item.increase_quality if item.expired?
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      item.writeoff if item.expired?
-    when 'Conjured Mana'
-      item.decrease_quality 2 if item.expired?
-    when 'Sulfuras, Hand of Ragnaros'
-      # Do nothing
-    else
-      item.decrease_quality if item.expired?
-    end
+    item.perform_inventory_rollover
+    item.perform_inventory_expiration
   end
 
 end
--- lib/inventory_item.rb
+++ lib/inventory_item.rb
@@ -31,4 +31,43 @@ class InventoryItem
     @item.quality = 0
   end
 
+  def perform_inventory_rollover
+    case name
+    when 'Aged Brie'
+      rollover
+      increase_quality
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      rollover
+      amount = case @item.sell_in
+      when (0..4) then 3
+      when (5..9) then 2
+      else 1
+      end
+      increase_quality amount
+    when 'Conjured Mana'
+      rollover
+      decrease_quality 2
+    when 'Sulfuras, Hand of Ragnaros'
+      # Do nothing
+    else
+      rollover
+      decrease_quality
+    end
+  end
+
+  def perform_inventory_expiration
+    case name
+    when 'Aged Brie'
+      increase_quality if expired?
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      writeoff if expired?
+    when 'Conjured Mana'
+      decrease_quality 2 if expired?
+    when 'Sulfuras, Hand of Ragnaros'
+      # Do nothing
+    else
+      decrease_quality if expired?
+    end
+  end
+
 end
